8895749cf14c48f2eb1a9077dfeb416414c37e48
hvr9 <70189888+hvr9@users.noreply.github.com>
2020-10-27 10:29:25 +0530

    Add files via upload

---
+++ L8-3.DoublyLLadd@end.py
@@ -0,0 +1,44 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.previous=None
+class DLinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def append(self,data):
+        new=node(data)
+        if(self.tail==None):
+            self.tail=self.head=new
+            self.tail.prev=None
+            self.tail.next=None
+        else:
+            self.tail.next=new
+            new.prev=self.tail
+            self.tail=new
+            self.tail.next=None
+    def disp(self):
+        cur=self.head
+        while cur:
+            print(cur.data)
+            cur=cur.next
+    def addatpos(self,data,prev):
+        cur=self.head
+        new=node(data)
+        while cur:
+            if(cur==prev):
+                cur.next=new
+                new.prev=cur
+                new.next=cur.next
+        cur=cur.next
+d=DLinkedList()
+n=int(input("size:"))
+for i in range(n):
+    k=int(input())
+    d.append(k)
+prev=int(input("enter prev node to insert:"))
+p=int(input("ele:"))
+d.addatpos(p,prev)
+d.disp()
+            
